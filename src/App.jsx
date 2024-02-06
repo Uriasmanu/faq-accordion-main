0985c12dcca7fe1401ca02aaec24365a88ff9c1a
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-02-06 11:28:00 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {styled} from  'styled-components';
+import backgrond from "../public/images/background-pattern-desktop.svg"
+import EstilosGlobais from "./componentes/estilosGlobais"
+
+const Conteiner = styled.div`
+  max-width: 1440px;
+  width: 100%;
+
+
+`
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p> FAQs
+    <Conteiner>
+    <EstilosGlobais/>
+    <img src={backgrond} alt="Background" style={{ width: '115%' }}/>
 
-What is Frontend Mentor, and how will it help me?
-
-Frontend Mentor offers realistic coding challenges to help developers improve their 
-frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-all levels and ideal for portfolio building.
-
-Is Frontend Mentor free?
-
-Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
-option providing access to a range of projects suitable for all skill levels.
-
-Can I use Frontend Mentor projects in my portfolio?
-
-Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-way to showcase your skills to potential employers!
-
-How can I get help if I'm stuck on a Frontend Mentor challenge?
-
-The best place to get help is inside Frontend Mentor's Discord community. There's a help 
-channel where you can ask questions and seek support from other community members.
-
-
-</p>
-<div class="attribution">
-  Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-  Coded by <a href="#">Your Name Here</a>.
-</div>
-    </>
+    </Conteiner>
   )
 }
 
